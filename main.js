61c36d7519cac7e91c7a1506725038ce1a2ea12e
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2024-10-18 20:21:40 +0700

    Edit:All Link to connect Projects

--- main.js
+++ main.js
@@ -19,33 +19,33 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 function goToRecipePage() {
-    window.open("http://127.0.0.1:5501/Recipe/Recipe.html", "_blank");  }
+    window.open("https://prapie96.github.io/PortFolioWeb/Recipe/Recipe.html", "_blank");  }
     
 
 function goToGridRecipePage() {
-    window.open("http://127.0.0.1:5501/GridEdit/Grid.html", "_blank");  }    
+    window.open("https://prapie96.github.io/PortFolioWeb/GridEdit/Grid.html", "_blank");  }    
 
 function goToTextToPage(){
-    window.open("http://127.0.0.1:5501/assignment5/index.html", "_blank");  
+    window.open("https://prapie96.github.io/PortFolioWeb/assignment5/index.html", "_blank");  
 }
 function goToRegisterPage() {
-    window.open("http://127.0.0.1:5501/assignment6/register.html", "_blank");  }
+    window.open("https://prapie96.github.io/PortFolioWeb/assignment6/register.html", "_blank");  }
     
 
 function goToLoginPage() {
-    window.open("http://127.0.0.1:5501/assignment6/login.html", "_blank");  }    
+    window.open("https://prapie96.github.io/PortFolioWeb/assignment6/login.html", "_blank");  }    
 
 function goToGameBoxage(){
-    window.open("http://127.0.0.1:5501/assignment7/index.html", "_blank");  
+    window.open("https://prapie96.github.io/PortFolioWeb/assignment7/index.html", "_blank");  
 }
 function goToSoulEscapePage(){
-    window.open("https://play.google.com/store/apps/details?id=com.Pie.Game");
+    window.open("https://prapie96.github.io/PortFolioWeb/Project.html");
 }
 function goToProjectPage(){
-    window.location.replace("http://127.0.0.1:5501/Project.html");
+    window.location.replace("https://prapie96.github.io/PortFolioWeb/Project.html");
 }
 function goToBackPage() {
-    window.location.href = "http://127.0.0.1:5501/index.html";
+    window.location.href = "https://prapie96.github.io/PortFolioWeb/index.html";
 }
 function goToRefBG() {
     window.open("https://img.freepik.com/free-vector/halftone-background-with-waves_23-2148907690.jpg?w=1380&t=st=1696874238~exp=1696874838~hmac=317837af9b57389d176f2fe10ca40f4dec4e239a0cc9604888119dc65d5f9179");
